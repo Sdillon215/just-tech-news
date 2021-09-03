8b56bce4873df37a32497c4ba7b3e519c24cde1d
Sean <sdillon215@gmail.com>
2021-09-02 17:22:48 -0700

    bcrypt

--- models/user.js
+++ models/user.js
@@ -1,64 +1,76 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
-  {
-    // TABLE COLUMN DEFINITIONS GO HERE
-    // define id column
-    id: {
-        // use the special sequelize datatypes objeect provide what type of data it is
-        type: DataTypes.INTEGER,
-        // this is the equivalent of sql's NOT NULL  option
-        allowNull: false,
-        // instruct that this is the primary key
-        primaryKey: true,
-        // turn on auto inc
-        autoIncrement: true
-    },
-    // define a username column
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    // define an email column
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // there cannot be duplicate emails
-        unique: true,
-        // if allowNull is false we can run our data through validator
-        validate: {
-            isEmail: true
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+        // define id column
+        id: {
+            // use the special sequelize datatypes objeect provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of sql's NOT NULL  option
+            allowNull: false,
+            // instruct that this is the primary key
+            primaryKey: true,
+            // turn on auto inc
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be duplicate emails
+            unique: true,
+            // if allowNull is false we can run our data through validator
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password length must be at least 4 chars long
+                len: [4]
+            }
         }
     },
-    // define a password column
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            // this means the password length must be at least 4 chars long
-            len: [4]
-        }
-    }
-  },
-  {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
 );
 
 module.exports = User;
