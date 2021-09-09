59a906aad7ffc1d844e72a9b9f70dcd335ff6d0b
Sean <sdillon215@gmail.com>
2021-09-08 20:10:15 -0700

    file names corrected

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import all models
-const Post = require('./notPost');
-const User = require('./notUser');
+const Post = require('./Post');
+const User = require('./User');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
 
