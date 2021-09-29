bfec97b0fecfb3755e3c980eb59bb59bc68a1d84
Sean <sdillon215@gmail.com>
2021-09-28 22:59:08 -0700

    bug fix

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -17,7 +17,8 @@ async function signupFormHandler(event) {
         });
         // check the response status
         if (response.ok) {
-            console.log('success');
+            document.location.replace('/dashboard');
+            
         } else {
             alert(response.statusText);
         }
